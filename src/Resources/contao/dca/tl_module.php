e5a90c14ac96deeb7f48a356dc097f53dbad213c
Mario Gienapp <mario.gienapp@postyou.de>
2020-01-15 13:17:12 +0100

    add dca

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_module']['palettes']['dlh_googlemaps_list_accordion_map'] =
+    '{title_legend},name,headline,type;{map_legend},dlh_googlemap,dlh_googlemap_size,dlh_googlemap_zoom,dlh_googlemap_protected,dlh_googlemap_static,dlh_googlemap_nocss,dlh_googlemap_tabs;{template_legend:hide},dlh_googlemap_template;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
